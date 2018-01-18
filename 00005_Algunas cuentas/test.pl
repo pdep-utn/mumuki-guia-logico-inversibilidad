6abfbc9f03820b8b4290ade0309ce67a2d7c08a9
franco@mumuki.org <franco@mumuki.org>
2018-01-18 07:26:04 -0500

    Mumuki Export on 2018-01-18 07:26:04 -0500

--- 00005_Algunas cuentas/test.pl
+++ 00005_Algunas cuentas/test.pl
@@ -1,12 +1,16 @@
 test('cuantaNaftaConsume(600, 40)') :-
-   cuantaNaftaConsume(600, 40).
+   cuantaNaftaConsume(600, X),
+   X =:= 40.
 
 test('cuantaNaftaConsume(1200, 80)') :-
-   cuantaNaftaConsume(1200, 80).
+   cuantaNaftaConsume(1200, X),
+   X =:= 80.
 
 test('cuantaNaftaConsume(60, 4)') :-
-   cuantaNaftaConsume(60, 4).
+   cuantaNaftaConsume(60, X),
+   X =:= 4.
 
 test('no cuantaNaftaConsume(60, 8)') :-
-   \+ (cuantaNaftaConsume(60, 8)).
+   cuantaNaftaConsume(60, X),
+   X =\= 8.
 
